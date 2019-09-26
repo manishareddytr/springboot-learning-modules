b1d22c8a54492a5a2e9e1028f09234a43f800d03
Manisha Reddy Thirumala Reddy <35759121+manishareddytr@users.noreply.github.com>
2019-09-26 11:56:00 +0530

    Add files via upload

--- demo/src/main/java/com/manisha/demo/StudentController.java
+++ demo/src/main/java/com/manisha/demo/StudentController.java
@@ -1,8 +1,6 @@
 package com.manisha.demo;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,59 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/students")
 public class StudentController {
 	
-	private StudentReport std;
-	private List<StudentReport> srl;
+	
+	private StudentService studService;
 	
 	@PostMapping("/")
 	public String updateMarks(@RequestBody StudentReport sr) {
-		String rst ="";
-		if(srl == null) {
-			srl=new ArrayList<>();
-		}
-		
-		for(StudentReport stdrpt: srl) {
-			if(sr.getId() == stdrpt.getId()) {
-				rst="Student cannot be added as id is present";
-			}
-		}
-		if(!(rst.equals(""))) return rst;
-		std=new StudentReport();
-		std.setId(sr.getId());
-		std.setName(sr.getName());
-		std.setChemistry(sr.getChemistry());
-		std.setMaths(sr.getMaths());
-		std.setPhysics(sr.getPhysics());
-		std.setAverage((std.getChemistry()+std.getMaths()+std.getPhysics())/3);
-		srl.add(std);
-		if(rst.equals("")) rst = "Successfully added the student with id:  "+sr.getId();
-		return rst;
+		return studService.updateStudentMarks(sr);
 	}
 	
 	@GetMapping("/{id}")
 	public StudentReport getStudentById(@PathVariable(value="id") int id) {
-		StudentReport sr = null;
-		for(StudentReport studobj: srl) {
-			if(id==studobj.getId()) {
-				sr=studobj;
-			} 
-		}
-		return sr;
+		return studService.getStudentbyId(id);
 	}
 	
 	@GetMapping("/")
 	public List<StudentReport> getAllStudent() {
-		return srl;
+		return studService.getAllStudents();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public String deleteStudentById(@PathVariable(value="id") int id) {
-		String rst="Id is not present";
-		for(StudentReport sr: srl) {
-			if(id==sr.getId()) {
-				rst="Succesfully deleted the student  "+id;
-				srl.remove(sr);
-			}
-		}
-		return rst;
+		return studService.deleteStudentbyId(id);
 	}
+	
 }
---
+++ demo/src/main/java/com/manisha/demo/StudentService.java
@@ -0,0 +1,56 @@
+package com.manisha.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentService {
+	private StudentReport std;
+	private List<StudentReport> srl;
+	public String updateStudentMarks(StudentReport sr) {
+		String rst ="";
+		if(srl == null) {
+			srl=new ArrayList<>();
+		}
+		if(sr==null) return "Please create student before requesting...";
+		
+		for(StudentReport stdrpt: srl) {
+			if(sr.getId() == stdrpt.getId()) {
+				rst="Student cannot be added as id is present";
+			}
+		}
+		if(!(rst.equals(""))) return rst;
+		std=new StudentReport();
+		std.setId(sr.getId());
+		std.setName(sr.getName());
+		std.setChemistry(sr.getChemistry());
+		std.setMaths(sr.getMaths());
+		std.setPhysics(sr.getPhysics());
+		std.setAverage((std.getChemistry()+std.getMaths()+std.getPhysics())/3);
+		srl.add(std);
+		if(rst.equals("")) rst = "Successfully added the student with id:  "+sr.getId();
+		return rst;
+	}
+	
+	public StudentReport getStudentbyId(int id) {
+		StudentReport sr = null;
+		for(StudentReport studobj: srl) {
+			if(id==studobj.getId()) {
+				sr=studobj;
+			} 
+		}
+		return sr;
+	}
+	public List<StudentReport> getAllStudents(){
+		return srl;
+	}
+	public String deleteStudentbyId(int id) {
+		String rst="Id is not present";
+		for(StudentReport sr: srl) {
+			if(id==sr.getId()) {
+				rst="Succesfully deleted the student  "+id;
+				srl.remove(sr);
+			}
+		}
+		return rst;
+	}
+}
